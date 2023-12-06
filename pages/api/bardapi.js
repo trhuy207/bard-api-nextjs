19078b7ab41225c5745e521a3e53ed37001b3677
williamtran207 <quochuyphbrvt07@gmail.com>
2023-12-06 20:41:36 +0700

    Update bardapi.js

--- pages/api/bardapi.js
+++ pages/api/bardapi.js
@@ -10,27 +10,27 @@ const client = new DiscussServiceClient({
 
 
 export default function handler(req, res) {
-    let messages = [{ content: req.query.ques }];
-  
-client.generateMessage({
-  // required, which model to use to generate the result
-  model: MODEL_NAME,
-  // optional, 0.0 always uses the highest-probability result
-  temperature: 0.25,
-  // optional, how many candidate results to generate
-  candidateCount: 1,
-  // optional, number of most probable tokens to consider for generation
-  top_k: 40,
-  // optional, for nucleus sampling decoding strategy
-  top_p: 0.95,
-  prompt: {
-    messages: messages,
-  },
-}).then(result => {
+  let messages = [{ content: req.query.ques }];
+
+  client.generateMessage({
+    // required, which model to use to generate the result
+    model: MODEL_NAME,
+    // optional, 0.0 always uses the highest-probability result
+    temperature: 0.25,
+    // optional, how many candidate results to generate
+    candidateCount: 1,
+    // optional, number of most probable tokens to consider for generation
+    top_k: 40,
+    // optional, for nucleus sampling decoding strategy
+    top_p: 0.95,
+    prompt: {
+      messages: messages,
+    },
+  }).then(result => {
     console.log("First Response:", result[0].candidates[0]?.content);
 
     messages.push({ content: result[0].candidates[0]?.content });
-   // console.log(JSON.stringify(result, null, 2));
+    // console.log(JSON.stringify(result, null, 2));
     res.status(200).json({ resp: messages })
-});
+  });
 }
